e43d759d15c18eda0e45643ce85eb06854162f2b
HP <HP@DESKTOP-AQ07QGH>
2024-02-16 10:39:10 +0530

    first commit

---
+++ Calc.cs
@@ -0,0 +1,22 @@
+﻿namespace CalcExample
+{
+    public class Calc
+    {
+        public void Add(double n1, double n2)
+        {
+            Console.WriteLine("Result after addition:\t"+(n1+n2));
+        }
+        public void Sub(double n1, double n2)
+        {
+            Console.WriteLine("Result after substraction:\t" + (n1 - n2));
+        }
+        public void Div(double n1, double n2)
+        {
+            Console.WriteLine("Result after division:\t" + (n1 / n2));
+        }
+        public void Multi(double n1, double n2)
+        {
+            Console.WriteLine("Result after multiplication:\t" + (n1 * n2));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CalcExample;
+
+Calc calc = new Calc();
+Console.WriteLine("Add call");
+calc.Add(12, 45);
